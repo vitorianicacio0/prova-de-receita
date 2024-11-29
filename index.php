1814ea676eb68a28d7829f395d767cd0e3958944
Lavyniao <lavyniao600@gmail.com>
2024-11-29 16:31:35 -0300

    torta

---
+++ index.php
@@ -0,0 +1,43 @@
+<h2>receita:
+    <?php
+     echo"torta de limão";
+     ?>
+     <h2>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>ingredientes</th>
+                    <th>quatindade</th>
+</tr>
+</thead>
+<tbody>
+    <tr>
+        <td>biscoito  maisena </td>
+        <td>200 gramas</td>
+</tr>
+<tr>
+        <td>manteiga ou margarina</td>
+        <td>2 colheres</td>
+</tr>
+<tr>
+        <td>creme de leite</td>
+        <td>200 gramas</td>
+</tr>
+<tr>
+        <td>suco de limão</td>
+        <td> 3 limãoes</td>
+</tr>
+
+<tr>
+        <td>claras de ovos</td>
+        <td>2 claras</td>
+</tr>
+<tr>
+        <td>açúcar</td>
+        <td>a gosto</td>
+</tr>
+<h1>nota:</h1>
+<script>
+    var h1=document.querySelector("h1");
+    h1.innerHTML="nota:10";
+    </script>
